b37996df4095f65dd06cc80d8d7f2a8b7c6ea6ae
caspianbryan <brianberrieron@gmail.com>
2024-01-17 00:06:55 +0300

    intergrated mongodb for persisting data

--- index.js
+++ index.js
@@ -1,80 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+require('dotenv').config()
 
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
-app.get('/info', (req,res) => {
-    res.send('<h1> Hello web, I am the world! </h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-    res.json(notes)
-})
-
-app.get('/api/notes/:id', (req,res) => {
-
-    const id = Number(req.params.id)
-    const note = notes.find(note => note.id === id)
-    if(note) {
-        res.json(note)
-    } else {
-        res.send('Check the address and try again').status(404).end()
-    }
-
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-    return maxId + 1
-}
-
-app.post('/api/notes', (req, res) => {
-    const body = req.body
-
-    if(!body.content) {
-        return res.status(400).json({
-            error: 'Content Missing'
-        })
-    }
-    const note = {
-        content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId()
-    }
-    
-    notes = notes.concat(note)
-
-    res.json(note)
-})
-
-app.delete('/api/notes/:id', (req, res) => {
-
-    const id = Number(req.params.id)
-    notes = notes.filter(note => note.id !== id)
-    res.status(204).end()
-
-})
+const Note = require('./models/note')
 
 const requestLogger = (req, res, next) => {
     console.log('Method:', req.method);
@@ -84,16 +13,89 @@ const requestLogger = (req, res, next) => {
     next()
 }
 
-app.use(requestLogger)
+const errorHandler = (error, req, res, next) => {
+    console.log(error.message);
+
+    if(error.message === 'CastError') {
+        return res.status(400).send(
+            {error: 'malformatted id'}
+        )
+    }
+    next(error)
+}
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({error: "Unknown endpoint"})
 }
 
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('dist'))
+
+app.get('/info', (req,res) => {
+    res.send('<h1> Hello web, I am the world! </h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end() 
+        }
+      })
+      .catch(error => next(error))
+})
+
+
+app.post('/api/notes', (req, res) => {
+    const body = req.body
+
+    if(body.content === undefined) {
+        return res.status(400).json({error: 'Content missing'})
+    }
+
+    const note = new Note ({
+        content: body.content,
+        important: Boolean(body.important) || false,
+    })
+    
+    note.save().then(savedNote => {
+        res.json(savedNote)
+    })
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+    const body = req.body
+
+    const note ={
+        content: body.content,
+        important: body.important,
+    }
+
+    Note.findByIdAndUpdate(req.params.id, note, {new: true}).then(updatedNote => {
+        res.json(updatedNote)
+    }).catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+    Note.findByIdAndDelete(req.params.id).then(result => {
+        res.status(204).end()
+    }).catch(error => next(error))
+})
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
-
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
---
+++ models/note.js
@@ -0,0 +1,29 @@
+var colors = require('colors');
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URL
+
+mongoose.connect(url)
+    .then(res => {
+        console.log('connected to MongoDB'.brightBlue);
+    })
+    .catch((err) => {
+        console.log('error connecting to MongoDB:', err.message);
+    })
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
