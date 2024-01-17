cd3c4802e577759310804cbb2c587fd6c0669d17
caspianbryan <brianberrieron@gmail.com>
2024-01-17 21:37:52 +0300

    final part then i deploy

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, req, res, next) => {
         return res.status(400).send(
             {error: 'malformatted id'}
         )
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message})
     }
     next(error)
 }
@@ -56,7 +58,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (req, res, next) => {
     const body = req.body
 
     if(body.content === undefined) {
@@ -70,20 +72,23 @@ app.post('/api/notes', (req, res) => {
     
     note.save().then(savedNote => {
         res.json(savedNote)
-    })
+    }).catch(err => next(err))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const body = req.body
+    const { content, important } = req.body
 
     const note ={
         content: body.content,
         important: body.important,
     }
 
-    Note.findByIdAndUpdate(req.params.id, note, {new: true}).then(updatedNote => {
-        res.json(updatedNote)
-    }).catch(error => next(error))
+    Note.findByIdAndUpdate(req.params.id,
+        { content, important },
+        { new: true, runValidators: true, context: 'query'}
+        ).then(updatedNote => {
+            res.json(updatedNote)
+        }).catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
