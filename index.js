1a2a9bc53d18838d21a81353d10cde1ca90590fc
caspianbryan <brianberrieron@gmail.com>
2024-01-22 18:21:00 +0300

    added linting rules

--- index.js
+++ index.js
@@ -6,28 +6,28 @@ require('dotenv').config()
 const Note = require('./models/note')
 
 const requestLogger = (req, res, next) => {
-    console.log('Method:', req.method);
-    console.log('Path:', req.path);
-    console.log('Body:', req.body);
-    console.log('---');
-    next()
+  console.log('Method:', req.method);
+  console.log('Path:', req.path);
+  console.log('Body:', req.body);
+  console.log('---');
+  next()
 }
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error.message);
+  console.log(error.message);
 
-    if(error.message === 'CastError') {
-        return res.status(400).send(
-            {error: 'malformatted id'}
-        )
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message})
-    }
-    next(error)
+  if(error.message === 'CastError') {
+    return res.status(400).send(
+      { error: 'malformatted id' }
+    )
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: "Unknown endpoint"})
+  res.status(404).send({ error: "Unknown endpoint" })
 }
 
 app.use(cors())
@@ -36,65 +36,65 @@ app.use(requestLogger)
 app.use(express.static('dist'))
 
 app.get('/info', (req,res) => {
-    res.send('<h1> Hello web, I am the world! </h1>')
+  res.send('<h1> Hello web, I am the world! </h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-    Note.find({}).then(notes => {
-        res.json(notes)
-    })
+  Note.find({}).then(notes => {
+    res.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-      .then(note => {
-        if (note) {
-          response.json(note)
-        } else {
-          response.status(404).end() 
-        }
-      })
-      .catch(error => next(error))
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 app.post('/api/notes', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    if(body.content === undefined) {
-        return res.status(400).json({error: 'Content missing'})
-    }
+  if(body.content === undefined) {
+    return res.status(400).json({ error: 'Content missing' })
+  }
 
-    const note = new Note ({
-        content: body.content,
-        important: Boolean(body.important) || false,
-    })
-    
-    note.save().then(savedNote => {
-        res.json(savedNote)
-    }).catch(err => next(err))
+  const note = new Note ({
+    content: body.content,
+    important: Boolean(body.important) || false,
+  })
+
+  note.save().then(savedNote => {
+    res.json(savedNote)
+  }).catch(err => next(err))
 })
 
 app.put('/api/notes/:id', (req, res, next) => {
-    const { content, important } = req.body
+  const { content, important } = req.body
 
-    const note ={
-        content: content,
-        important: important,
-    }
+  const note ={
+    content: content,
+    important: important,
+  }
 
-    Note.findByIdAndUpdate(req.params.id,
-        note,
-        { new: true, runValidators: true, context: 'query'}
-        ).then(updatedNote => {
-            res.json(updatedNote)
-        }).catch(error => next(error))
+  Note.findByIdAndUpdate(req.params.id,
+    note,
+    { new: true, runValidators: true, context: 'query' }
+  ).then(updatedNote => {
+    res.json(updatedNote)
+  }).catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (req, res, next) => {
-    Note.findByIdAndDelete(req.params.id).then(result => {
-        res.status(204).end()
-    }).catch(error => next(error))
+  Note.findByIdAndDelete(req.params.id).then(result => {
+    res.status(204).end()
+  }).catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
@@ -102,5 +102,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- models/note.js
+++ models/note.js
@@ -6,28 +6,28 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URL
 
 mongoose.connect(url)
-    .then(res => {
-        console.log('connected to MongoDB'.brightBlue);
-    })
-    .catch((err) => {
-        console.log('error connecting to MongoDB:', err.message);
-    })
+  .then(res => {
+    console.log('connected to MongoDB'.brightBlue);
+  })
+  .catch((err) => {
+    console.log('error connecting to MongoDB:', err.message);
+  })
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
